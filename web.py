9856d90da5696704a5234a08e3ebebd0dbc019bd
rodr1g <61032108+rodr1g@users.noreply.github.com>
2020-04-16 22:14:28 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Rodrigo Coutinho"
+    return "SE VOCÊ JA DEU A BUNDINHA, DE UMA RISADINHA  HAHAHA"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
